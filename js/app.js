2202452274f228a90c1565bad998d08fa118eeac
jth4 <ohi@jth4.com>
2017-05-30 18:07:32 -0700

    Build Up
    Basic structure, some styles and js

---
+++ js/app.js
@@ -0,0 +1,90 @@
+//////////////////////
+// Alert
+//////////////////////
+
+const close = document.getElementById('alertClose');
+
+close.addEventListener('click', function () {
+	const alert = document.getElementById('alert');
+	alert.innerHTML = "You're all caught up!";
+	
+	function changeAlertStatus(){ 
+		alert.className = "noAlerts";}
+	
+	setTimeout(changeAlertStatus, 500);
+});
+
+
+// Charts
+
+//////////////////////
+// Visitors Chart
+//////////////////////
+
+var ctx = document.getElementById('trafficChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+//////////////////////
+//Daily Traffic Chart
+//////////////////////
+
+var ctx = document.getElementById('dailyTrafficChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+//////////////////////
+// Mobile Users Chart
+//////////////////////
+
+var ctx = document.getElementById('mobileUsersChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
